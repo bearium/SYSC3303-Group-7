46a1a05700c036d549377a4d0b084d88852e8e1d
thomasbryk <thomasbryk@cmail.carleton.ca>
2019-01-31 17:24:11 -0500

    FloorSubsystem now sends requests. Lamp requests needs implementation.

--- src/main/floorSubsystem/FloorSubsystem.java
+++ src/main/floorSubsystem/FloorSubsystem.java
@@ -12,6 +12,8 @@ import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -22,11 +24,9 @@ import java.util.Locale;
 import java.util.Queue;
 
 import main.ElevatorSystemComponent;
-import main.global.Direction;
-import main.global.ElevatorSystemConfiguration;
-import main.server.Server;
-import shared.FloorButtonRequest;
-import shared.Request;
+import main.global.*;
+import main.requests.*;
+import main.server.*;
 
 public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 
@@ -40,16 +40,17 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 	private Thread serverThread;
 	private String name;
 	private Queue<Request> requestsQueue;
+	private int schedulerPort;
 	private boolean debug = false;
+	private static String requestsFile = "resources/requests.txt";
 
-	public FloorSubsystem(String name, int port) {
+	public FloorSubsystem(String name, int port, int schedulerPort) {
 		this.name = name;
 		this.requestsQueue = new LinkedList<Request>();
+		this.schedulerPort = schedulerPort;
 
-		// Create a server (bound to this Instance of ElevatorSubsystem) in a new
-		// thread.
-		// When this server receives requests, they will be added to the eventsQueue of
-		// THIS ElevatorSubsystem instance.
+		// Create a server (bound to this Instance of FloorSubsystem) in a new thread.
+		// When this server receives requests, they will be added to the requestsQueue of this FloorSubsystem instance.
 		server = new Server(this, port, this.debug);
 		serverThread = new Thread(server, name);
 		serverThread.start();
@@ -86,7 +87,7 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 		}
 	}
 
-	private static Date getTime(String s) {
+	private static Date convertTime(String s) {
 		DateFormat format = new SimpleDateFormat("hh:mm:ss.SSS", Locale.ENGLISH);
 		try {
 			return format.parse(s);
@@ -99,7 +100,7 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 	private static List<FloorButtonRequest> readInputFromFile() {
 		FileReader input = null;
 		try {
-			input = new FileReader("requests.txt");
+			input = new FileReader(requestsFile);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -113,7 +114,7 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 			while ((myLine = bufRead.readLine()) != null) {
 				String[] info = myLine.split(" ");
 
-				Date time = getTime(info[0]);
+				String time = info[0];
 				String floorName = info[1];
 				Direction direction = getDirection(info[2]);
 				String destinationFloor = info[3];
@@ -131,23 +132,32 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 	@Override
 	public void run() {
 		while (true) {
-			// After each event check to see if the next event has been set
-			Request event = this.getNextEvent();
-			if (event != null) {
-				System.out.println(this.name + ": Event received: " + event);
-				System.out.println(this.name + ": Simulating: " + event + " for 5 seconds...");
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println(this.name + ": Simulation of " + event + " complete");
+			this.handleEvent(this.getNextEvent());
+		}
+	}
+	
+	private void handleEvent(Request event) {
+		//switch statement corresponding to different "event handlers"
+		if (event instanceof FloorButtonRequest) {
+			FloorButtonRequest request = (FloorButtonRequest) event;
+			try {
+				this.server.send(request, InetAddress.getLocalHost(), schedulerPort);
+				consoleOutput("Sending floor trip request.");
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
 			}
 		}
+	}
+	
+	private static void consoleOutput(String output) {
+		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss.S")) + "] " + output);
 	}
 
 	public static void main(String[] args) {
 		List<FloorSubsystem> floors = new LinkedList<FloorSubsystem>();
+		
+		//This will return a Map of all attributes for the Scheduler (as per config.xml)
+		HashMap<String, String> schedulerConfiguration = ElevatorSystemConfiguration.getSchedulerConfiguration();
 
 		// This will return a Map of Maps. First key -> floor Name, Value -> map of
 		// all attributes for that floor (as per config.xml)
@@ -161,7 +171,7 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 
 			// Create an instance of floorSubsystem for this 'floorName'
 			FloorSubsystem floorSubsystem = new FloorSubsystem(floorName,
-					Integer.parseInt(floorConfiguration.get("port")));
+					Integer.parseInt(floorConfiguration.get("port")), Integer.parseInt(schedulerConfiguration.get("port")));
 			floors.add(floorSubsystem);
 
 			// Spawn and start a new thread for this floorSubsystem instance
@@ -174,9 +184,12 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 		Collections.sort(requests, new Comparator<FloorButtonRequest>() {
 			@Override
 			public int compare(FloorButtonRequest r1, FloorButtonRequest r2) {
-				if (r1.Time.after(r2.Time))
+				Date r1Time = convertTime(r1.getTime());
+				Date r2Time = convertTime(r2.getTime());
+				
+				if (r1Time.after(r2Time))
 					return 1;
-				else if (r1.Time.before(r2.Time))
+				else if (r1Time.before(r2Time))
 					return -1;
 				else
 					return 0;
@@ -186,18 +199,22 @@ public class FloorSubsystem implements Runnable, ElevatorSystemComponent {
 		long lastTime = 0;
 
 		for (FloorButtonRequest currRequest : requests) {
+			
 			for (FloorSubsystem currFloor : floors) {
-				if (currFloor.getName().equalsIgnoreCase(currRequest.FloorName)) {
+				if (currFloor.getName().equalsIgnoreCase(currRequest.getFloorName())) {
+					long currReqTime = (convertTime(currRequest.getTime())).getTime();
+					
 					if (lastTime != 0) {
-						long timeDiff = currRequest.Time.getTime() - lastTime;
+						long timeDiff = currReqTime - lastTime;
 						try {
 							Thread.sleep(timeDiff);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
+					consoleOutput("Request details // Time:" + currRequest.getTime() + "  Floor Name: " + currRequest.getFloorName() + "  Direction: " + currRequest.getDirection() + "  Dest Floor: " + currRequest.getDestinationFloor());
 					currFloor.receiveEvent(currRequest);
-					lastTime = currRequest.Time.getTime();
+					lastTime = currReqTime;
 				}
 			}
 		}
