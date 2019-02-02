355b08f43e2a5def2bb29047fe09cd973b78ac98
dillonclaremont <dillonclaremont@10.0.1.25>
2019-02-02 13:21:36 -0500

    standardize output amongst elevatorSubsystem/floorSubsystem/scheduler

--- src/main/ElevatorSystemComponent.java
+++ src/main/ElevatorSystemComponent.java
@@ -3,6 +3,9 @@ package main;
 import main.requests.Request;
 
 public interface ElevatorSystemComponent {
+	public final static String EVENT_RECEIVED = "[EVENT RECEIVED]";
+	public final static String EVENT_SENT = "[EVENT SENT]";
+	public final static String ACTION = "[ACTION]";
 	
 	public void receiveEvent(Request event);
 	public Request getNextEvent();
