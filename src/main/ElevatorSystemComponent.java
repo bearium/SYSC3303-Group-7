ca4526a68cc9ec6223e5f661fe910e1158e396c0
dillonclaremont <dillonclaremont@10.0.1.25>
2019-02-02 16:30:13 -0500

    finish up documentation

--- src/main/ElevatorSystemComponent.java
+++ src/main/ElevatorSystemComponent.java
@@ -3,10 +3,6 @@ package main;
 import main.requests.Request;
 
 public interface ElevatorSystemComponent {
-	public final static String EVENT_RECEIVED = "[EVENT RECEIVED]";
-	public final static String EVENT_SENT = "[EVENT SENT]";
-	public final static String ACTION = "[ACTION]";
-	
 	public void receiveEvent(Request event);
 	public Request getNextEvent();
 	public String getName();
--- src/main/scheduler/Scheduler.java
+++ src/main/scheduler/Scheduler.java
@@ -463,13 +463,19 @@ public class Scheduler implements Runnable, ElevatorSystemComponent {
 	}
 	
 	/**
-	 * 
+	 * Print to console in a specific format.
 	 * @param output
 	 */
 	private void consoleOutput(String output) {
 		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss.S")) + "] " + this.name + " : " + output);
 	}
 	
+	/**
+	 * Print to console a send/receive event in a specific format.
+	 * @param event
+	 * @param target
+	 * @param output
+	 */
 	private void consoleOutput(RequestEvent event, String target, String output) {
 		if (event.equals(RequestEvent.SENT)) {
 			System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss.S")) + "] " + this.name + " : [EVENT SENT TO " + target + "] " + output);
