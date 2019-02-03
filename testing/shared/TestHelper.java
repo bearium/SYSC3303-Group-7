d453a2678b9b89fb83abde19b1f675e2f1aeeb62
Gordon MacDonald <gordonlmacdonald@cmail.carleton.ca>
2019-02-02 19:07:49 -0500

    Update main UML diagram.

--- testing/shared/TestHelper.java
+++ testing/shared/TestHelper.java
@@ -1,27 +1,39 @@
 package shared;
-
+import main.requests.Helper;
 public class TestHelper {
 	
-	
+	private Helper helper = new Helper();
 	
 	
 	@Before
 	public void setup(){
-		DirectionLampRequest dlr = new DirectionLampRequest;
+		
+		
+		static DirectionLampRequest dirLR = new DirectionLampRequest(Direction.UP, Direction.ON);
+		static ElevatorArrivalRequest elAR = new ElevatorArrivalRequest("elevator1", "floor1");
+		static ElevatorDoorRequest elDR = new ElevatorDoorRequest("elevator1", ElevatorDoorStatus.OPENED);
+		static ElevatorLampRequest elLR = new ElevatorLampRequest("button", LampStatus.ON);
+		static ElevatorMotorRequest elMR = new ElevatorMotorRequest("elevator1", Direction.UP);
+		static FloorButtonRequest flBR = new FloorButtonRequest("time", "floor1", Direction.UP, "floor2");
+		static FloorLampRequest flLR = new FloorLampRequesst(direction.UP, LampStatus.ON);
 		
 	}
 	
 	
 	@Test
-	public void testCreateRequest(){
-		//attempt to create DatagramPackets from instances of Request using createRequest **consult mustafa about naming
-		//verify output
+	public void testCreateAndParseRequest(){
+		//attempt to create DatagramPackets from instances of Request using createRequest 
+		PacketdirLR = helper.CreateRequest(dirLR);
+		PacketelAR = helper.CreateRequest(elAR);
+		PacketelDR = helper.CreateRequest(elDR);
+		PacketelLR = helper.CreateRequest(elLR);
+		PacketelMR = helper.CreateRequest(elMR);
+		PacketflBR = helper.CreateRequest(flBR);
+		PacketflLR = helper.CreateRequest(flLR);
+		
+		//verify output of parse
+		assertEquals("Datagram creation and parsing experienced data loss", dirLR, helper.ParseRequest());
+		
 	}
 	
-	@Test
-	public void testParseRequest(){
-		//create packets from request if testCreateRequest passes
-		//attempt to parse DatagramPacket using parseRequest
-		//verify output
-	}
 }
