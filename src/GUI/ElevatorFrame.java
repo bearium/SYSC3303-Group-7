8fb188a2b1e738c862622c5c21eb53c8cc523ea2
4iner <foreigncode@gmail.com>
2019-04-09 20:52:15 -0400

    Centered Trip Tip

--- src/GUI/ElevatorFrame.java
+++ src/GUI/ElevatorFrame.java
@@ -6,7 +6,10 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.util.HashMap;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import main.elevatorSubsystem.ElevatorState;
@@ -21,15 +24,24 @@ public class ElevatorFrame extends JFrame{
 	
 	public ElevatorFrame(HashMap<String, ElevatorMonitor> elevatorMonitors) {
 		super("Elevator GUI Monitor");
-		this.setLayout(new GridBagLayout());
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		//this.setLayout(new GridBagLayout());
+		this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
 		for(ElevatorMonitor monitor : elevatorMonitors.values()) {
 			ElevatorState state = monitor.getElevatorState();
 			ElevatorMainPanel EMP =new ElevatorMainPanel(state);
-			this.add(EMP);
+			panel.add(EMP);
 			ElevatorTripPanel tp = new ElevatorTripPanel(monitor);
 			EMP.add(new JScrollPane(tp));
-			System.out.println("Adding elevator");
+			
 		}
+		panel.setBackground(SharedSettings.background_color);
+		this.add(panel);
+		JLabel label = new JLabel("DoubleClick on a trip request to view additional details", JLabel.CENTER);
+		label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		this.add(label);
+		this.add(new JLabel(" "));
 		initializeJFrame();
 	}
 	
